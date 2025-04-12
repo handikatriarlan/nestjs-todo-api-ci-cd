1d534b4bd9a8d39ebff51a86af5aee537fcc0915
handikatriarlan <handikaarlan@gmail.com>
2025-04-12 10:17:12 +0700

    feat(todo): add todo api endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TodoModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Todo, TodoDocument } from './schemas/todo.schema';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(@InjectModel(Todo.name) private todoModel: Model<TodoDocument>) {}
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    return this.todoModel.create(createTodoDto);
+  }
+
+  async findAll(): Promise<Todo[]> {
+    return this.todoModel.find().sort({ createdAt: -1 });
+  }
+
+  async findOne(id: string): Promise<Todo> {
+    const todo = await this.todoModel.findById(id);
+    if (!todo) throw new NotFoundException(`Todo with ID ${id} not found`);
+    return todo;
+  }
+
+  async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+    const updated = await this.todoModel.findByIdAndUpdate(id, updateTodoDto, {
+      new: true,
+    });
+    if (!updated) throw new NotFoundException(`Todo with ID ${id} not found`);
+    return updated;
+  }
+
+  async remove(id: string): Promise<void> {
+    const result = await this.todoModel.findByIdAndDelete(id);
+    if (!result) throw new NotFoundException(`Todo with ID ${id} not found`);
+  }
+}
